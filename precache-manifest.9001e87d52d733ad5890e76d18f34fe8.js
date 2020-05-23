bad05747023343cd927bcbee6e881c5f19d0d315
AmjadNas <amjadn1092@gmail.com>
2020-05-23 22:29:59 +0300

    Updates

---
+++ precache-manifest.9001e87d52d733ad5890e76d18f34fe8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2280dc138b507a133f7d49b758b60e46",
+    "url": "/word2vecinterface/index.html"
+  },
+  {
+    "revision": "c7a0a082ffafb9c4070f",
+    "url": "/word2vecinterface/static/js/2.b18fcda7.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/word2vecinterface/static/js/2.b18fcda7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b36bd54041310a9a8ea3",
+    "url": "/word2vecinterface/static/js/main.ac0e0941.chunk.js"
+  },
+  {
+    "revision": "9f2ed6e4e11c245c0f36",
+    "url": "/word2vecinterface/static/js/runtime-main.e2aa490c.js"
+  }
+]);
